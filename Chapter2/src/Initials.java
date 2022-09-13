839204284e0b536b86780c1bcf79479f004aca37
16076006 <16076006@L815WD220013.edu.cbe.ab.ca>
2022-09-13 08:58:01 -0600

    8:57am

---
+++ Chapter2/src/Initials.java
@@ -0,0 +1,43 @@
+/*
+
+Program: Initials.java          Last Date of this Revision: September 12, 2022
+
+Purpose: An application that displays my initials using line art.
+
+Author: Seth McKenzie 
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+public class Initials 
+{
+
+    public static void main(String args[])
+    {
+    	
+		System.out.println("------    ||     ||");
+		System.out.println("|         | |   | |");
+		System.out.println("|         |  | |  |");
+		System.out.println("------    |   |   |");
+		System.out.println("     |    |       |");
+		System.out.println("     |    |       |");
+		System.out.println("------    |       |");
+       
+    }
+}
+
+
+/* Screen Dump
+ 
+------    ||     ||
+|         | |   | |
+|         |  | |  |
+------    |   |   |
+     |    |       |
+     |    |       |
+------    |       |
+
+ 
+ */
--- Chapter2/src/TicTacToe.java
+++ Chapter2/src/TicTacToe.java
@@ -1,9 +1,50 @@
+/*
 
-public class TicTacToe {
+Program: TicTacToe.java          Last Date of this Revision: September 12, 2022
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Purpose: An application that displays a tic-tac-toe board as well as the rules for the game.
 
-	}
+Author: Seth McKenzie 
+School: CHHS
+Course: Computer Science 20
+ 
 
+*/
+
+public class TicTacToe 
+{
+
+    public static void main(String args[])
+    {
+    	
+    	System.out.println("      |       |");
+    	System.out.println("      |       |");
+    	System.out.println("      |       |");
+    	System.out.println("---------------------");
+    	System.out.println("      |       |");
+    	System.out.println("      |   X   |");
+    	System.out.println("      |       |");
+    	System.out.println("---------------------");
+    	System.out.println("      |       |");
+    	System.out.println("      |       |");
+    	System.out.println("      |       |");
+       
+    }
 }
+
+
+/* Screen Dump
+ 
+      |       |
+      |       |
+      |       |
+---------------------
+      |       |
+      |   X   |
+      |       |
+---------------------
+      |       |
+      |       |
+      |       |
+ 
+ */
