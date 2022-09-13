94a9cca88c0e4747abd407c886a750a249ede6b7
16076006 <16076006@L815WD220013.edu.cbe.ab.ca>
2022-09-13 09:49:05 -0600

    9:49am

---
+++ Chapter2/src/CRT.java
@@ -0,0 +1,41 @@
+
+public class CRT 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		/*
+		 
+	 		3:
+			/*
+
+			Program: TestAverage.java
+			Calculates test averages
+			Seth McKenzie
+			Date: 13/09/2022
+			
+			4:
+			//This class displays the averages of a set of imputed test scores
+
+			5:
+			package gradeCalculator
+
+			6:
+			Public class AverageScore
+
+			7:
+			Source code: the code that the programer types.
+
+			Bytecode: the source code that gets compiled by the computer into binary machine code that the hardware can understand.
+
+			12:
+			When print() is used to print a message the text cursor is placed at the end of the message on the same line. 
+			When println() is used to print a message the text cursor is placed at the start of the next line, as if someone pressed 
+			the enter key when typing.
+
+		 */
+		
+	}
+
+}
